e7579f365ab5d2cdabd17d80b670553f3db51ce4
Carlos Juarez <carlosgj94@gmail.com>
2024-04-23 17:24:36 +0200

    test: proposal creation working within the happy path

--- test/PolygonMultisig.t.sol
+++ test/PolygonMultisig.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.24;
 
 import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {IDAO} from "@aragon/osx/core/plugin/PluginUUPSUpgradeable.sol";
 
 import {DaoUnauthorized} from "@aragon/osx/core/utils/auth.sol";
 import {AragonTest} from "./base/AragonTest.sol";
@@ -22,6 +23,7 @@ abstract contract PolygonMultisigTest is AragonTest {
         });
 
     function setUp() public virtual {
+        vm.prank(address(0xB0b));
         setup = new PolygonMultisigSetup();
         bytes memory setupData = abi.encode(members, multisigSettings);
 
@@ -29,6 +31,8 @@ abstract contract PolygonMultisigTest is AragonTest {
 
         dao = _dao;
         plugin = PolygonMultisig(_plugin);
+        vm.roll(block.number + 1);
+        // vm.warp(1);
     }
 }
 
@@ -58,8 +62,26 @@ contract PolygonMultisigInitializeTest is PolygonMultisigTest {
     }
 }
 
-contract PolygonMultisigMembersTest is PolygonMultisigTest {
+contract PolygonMultisigProposalCreationTest is PolygonMultisigTest {
     function setUp() public override {
         super.setUp();
+    }
+
+    function test_createProposal() public {
+        vm.prank(address(0xB0b));
+        IDAO.Action memory _action = IDAO.Action({to: address(0x0), value: 0, data: bytes("0x00")});
+        IDAO.Action[] memory _actions = new IDAO.Action[](1);
+        _actions[0] = _action;
+        plugin.createProposal({
+            _metadata: bytes("ipfs://hello"),
+            _actions: _actions,
+            _allowFailureMap: 0,
+            _approveProposal: false,
+            _tryExecution: false,
+            _startDate: uint64(0),
+            _endDate: uint64(block.timestamp + 1 days),
+            _emergency: false
+        });
+        assertEq(plugin.proposalCount(), 1);
     }
 }
