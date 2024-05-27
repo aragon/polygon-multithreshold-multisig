54c9fe837175d3c372d117af4724373443022c31
Carlos Juarez <carlosgj94@gmail.com>
2024-05-27 18:34:45 +0200

    fix: adding test for too early confirmation

--- test/PolygonMultisig.t.sol
+++ test/PolygonMultisig.t.sol
@@ -333,6 +333,19 @@ contract PolygonMultisigConfirmation is PolygonMultisigTest {
         );
         plugin.confirm(0);
     }
+
+    function test_reverts_confirmation_if_too_early() public {
+        vm.startPrank(address(0xB0b));
+        plugin.approve(0);
+        plugin.startProposalDelay(0, bytes("ipfs://world"));
+        ( , , , uint64 _delayDuration) = plugin.multisigSettings();
+        vm.warp(block.timestamp + _delayDuration - 1);
+        assertEq(plugin.canConfirm(0, address(0xB0b)), false);
+        vm.expectRevert(
+            abi.encodeWithSelector(PolygonMultisig.ConfirmationCastForbidden.selector, 0, address(0xB0b))
+        );
+        plugin.confirm(0);
+    }
     function test_reverts_confirmation_if_no_member() public {
         vm.startPrank(address(0xB0b));
         plugin.approve(0);
